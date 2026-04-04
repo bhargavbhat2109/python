645cb670f18301080a3d5f5df919f17e45a96acb
Bhargav Bhat <bhargavbhat@Bhargavs-MacBook-Pro.local>
2026-04-04 16:07:49 +0530

    my first code

---
+++ 1.py
@@ -0,0 +1,8 @@
+required_number = [10, 8, 156, 399, 554, 4553, 4785, 5747, 9]
+for index, i in enumerate(required_number):
+    if index == 5:
+        print(required_number[index])
+    
+
+    
+    
