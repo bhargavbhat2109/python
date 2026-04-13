a04abd467562746cf5d23a270044840d80b6c78f
Bhargav Bhat <bhargavbhat@MacBook-Pro.local>
2026-04-13 09:38:16 +0530

    watter reminder

---
+++ waterreminder.py
@@ -0,0 +1,10 @@
+a=int(input("enter number of glasses of water you have drunk today: "))
+if a==5:
+    print("you are almost there!")
+elif a==8:
+    print("you are doing great!")
+elif a>8:
+    print("you are hydrated enogh for the day!")
+else :
+    print("come on man is summer!! You got to drink more water")
+    
